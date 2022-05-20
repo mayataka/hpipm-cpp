f6e0bdc6d4b237d48e0be856e624dbf5c4a7ae0a
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-05-20 20:49:43 +0900

    add solver stats

--- examples/example_ocp_qp_x0emb.cpp
+++ examples/example_ocp_qp_x0emb.cpp
@@ -117,9 +117,8 @@ int main() {
     std::cout << "pi[" << i << "]: " << sol.pi[i].transpose() << std::endl;
   }
 
-  // TODO: this causes segfault
-  // const auto stat = ipm.getStat();
-  // std::cout << stat << std::endl; 
+  const auto stat = ipm.getStat();
+  std::cout << stat << std::endl; 
 
   return 0;
 }

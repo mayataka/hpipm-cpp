c336faec82f3612a229f5fc4804cfd39a4f827e8
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-08-24 13:22:28 +0900

    update examples

--- examples/example_mpc.cpp
+++ examples/example_mpc.cpp
@@ -9,29 +9,20 @@
 
 int main() {
   // setup QP
-  hpipm::OcpQpDim dim;
-  dim.N    = 20;
-  dim.nx   = std::vector<int>(dim.N+1, 12);
-  dim.nu   = std::vector<int>(dim.N, 4);
-  dim.nbx  = std::vector<int>(dim.N+1, 3);
+  const int N = 20;
+  hpipm::OcpQpDim dim(N);
+  dim.nx   = std::vector<int>(N+1, 12);
+  dim.nu   = std::vector<int>(N, 4);
+  dim.nbx  = std::vector<int>(N+1, 3);
   dim.nbx[0] = 12;
-  dim.nbu  = std::vector<int>(dim.N, 4);
-  dim.ng   = std::vector<int>(dim.N+1, 0);
-  dim.nsbx = std::vector<int>(dim.N+1, 0);
-  dim.nsbu = std::vector<int>(dim.N, 0);
-  dim.nsg  = std::vector<int>(dim.N+1, 0);
-  const auto dim_err_msg = dim.checkSize();
-  if (!dim_err_msg.empty()) {
-    for (const auto& e : dim_err_msg) {
-      std::cout << e << std::endl;
-    }
-    return 1;
-  }
-  dim.createHpipmData();
+  dim.nbu  = std::vector<int>(N, 4);
+  dim.ng   = std::vector<int>(N+1, 0);
+  dim.nsbx = std::vector<int>(N+1, 0);
+  dim.nsbu = std::vector<int>(N, 0);
+  dim.nsg  = std::vector<int>(N+1, 0);
 
-  hpipm::OcpQp qp;
+  hpipm::OcpQp qp(dim);
   // initial state
-  qp.x0.resize(12);
   qp.x0 << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
   // dynamics
   Eigen::MatrixXd A(12, 12), B(12, 4);
@@ -60,10 +51,10 @@ int main() {
        0.0236,   0.,     -0.0236, 0.    ,
        0.2107,   0.2107,  0.2107, 0.2107;
   const Eigen::VectorXd b = Eigen::VectorXd::Zero(12);
-  for (int i=0; i<dim.N; ++i) {
-    qp.A.push_back(A);
-    qp.B.push_back(B);
-    qp.b.push_back(b);
+  for (int i=0; i<N; ++i) {
+    qp.A[i] = A;
+    qp.B[i] = B;
+    qp.b[i] = b;
   }
   // cost
   Eigen::MatrixXd Q(12, 12), S(4, 12), R(4, 4);
@@ -74,99 +65,69 @@ int main() {
   x_ref <<  0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0;
   const Eigen::VectorXd q = - Q * x_ref;
   const Eigen::VectorXd r = Eigen::VectorXd::Zero(4);
-  for (int i=0; i<dim.N; ++i) {
-    qp.Q.push_back(Q);
-    qp.R.push_back(R);
-    qp.S.push_back(S);
-    qp.q.push_back(q);
-    qp.r.push_back(r);
+  for (int i=0; i<N; ++i) {
+    qp.Q[i] = Q;
+    qp.R[i] = R;
+    qp.S[i] = S;
+    qp.q[i] = q;
+    qp.r[i] = r;
   }
-  qp.Q.push_back(Q);
-  qp.q.push_back(q);
+  qp.Q[N] = Q;
+  qp.q[N] = q;
   // constraints
   const bool use_mask_for_one_sided_constraints = true;
-  qp.idxbx.push_back({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11});
-  qp.lbx.push_back(qp.x0);
-  qp.ubx.push_back(qp.x0);
+  qp.idxbx[0] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+  qp.lbx[0] = qp.x0;
+  qp.ubx[0] = qp.x0;
   if (use_mask_for_one_sided_constraints) {
-    qp.ubx_mask.push_back(Eigen::VectorXd()); // there is no mask on initial stage
+    qp.ubx_mask[0] = Eigen::VectorXd(); // there is no mask on initial stage
   }
-  for (int i=1; i<=dim.N; ++i) {
+  for (int i=1; i<=N; ++i) {
     constexpr double soft_inf = 1.0e10;
-    qp.idxbx.push_back({0, 1, 5});
-    qp.lbx.push_back((Eigen::VectorXd(3) << -M_PI/6.0, -M_PI/6.0, -1.0).finished());
-    qp.ubx.push_back((Eigen::VectorXd(3) << M_PI/6.0, M_PI/6.0, soft_inf).finished());
+    qp.idxbx[i] = {0, 1, 5};
+    qp.lbx[i] = (Eigen::VectorXd(3) << -M_PI/6.0, -M_PI/6.0, -1.0).finished();
+    qp.ubx[i] = (Eigen::VectorXd(3) << M_PI/6.0, M_PI/6.0, soft_inf).finished();
     if (use_mask_for_one_sided_constraints) {
-      qp.ubx_mask.push_back((Eigen::VectorXd(3) << 1.0, 1.0, 0.0).finished()); 
+      qp.ubx_mask[i] = (Eigen::VectorXd(3) << 1.0, 1.0, 0.0).finished(); 
       // this mask disables upper bound by ubx[2]
     }
   }
-  for (int i=0; i<dim.N; ++i) {
+  for (int i=0; i<N; ++i) {
     constexpr double u0 = 10.5916;
-    qp.idxbu.push_back({0, 1, 2, 3});
-    qp.lbu.push_back((Eigen::VectorXd(4) << 9.6-u0, 9.6-u0, 9.6-u0, 9.6-u0).finished());
-    qp.ubu.push_back((Eigen::VectorXd(4) << 13.0-u0, 13.0-u0, 13.0-u0, 13.0-u0).finished());
+    qp.idxbu[i] = {0, 1, 2, 3};
+    qp.lbu[i] = (Eigen::VectorXd(4) << 9.6-u0, 9.6-u0, 9.6-u0, 9.6-u0).finished();
+    qp.ubu[i] = (Eigen::VectorXd(4) << 13.0-u0, 13.0-u0, 13.0-u0, 13.0-u0).finished();
   }
-  const auto qp_err_msg = qp.checkSize(dim);
-  if (!qp_err_msg.empty()) {
-    for (const auto& e : qp_err_msg) {
-      std::cout << e << std::endl;
-    }
-    return 1;
-  }
-  qp.createHpipmData(dim);
 
-  hpipm::OcpQpIpmSolverSettings arg;
-  arg.iter_max = 30;
-  arg.alpha_min = 1e-8;
-  arg.mu0 = 1e2;
-  arg.tol_stat = 1e-04;
-  arg.tol_eq = 1e-04;
-  arg.tol_ineq = 1e-04;
-  arg.tol_comp = 1e-04;
-  arg.reg_prim = 1e-12;
-  arg.warm_start = 1;
-  arg.pred_corr = 1;
-  arg.ric_alg = 0;
-  arg.split_step = 1;
-  arg.createHpipmData(dim);
+  hpipm::OcpQpIpmSolverSettings ipm_solver_settings;
+  ipm_solver_settings.iter_max = 30;
+  ipm_solver_settings.alpha_min = 1e-8;
+  ipm_solver_settings.mu0 = 1e2;
+  ipm_solver_settings.tol_stat = 1e-04;
+  ipm_solver_settings.tol_eq = 1e-04;
+  ipm_solver_settings.tol_ineq = 1e-04;
+  ipm_solver_settings.tol_comp = 1e-04;
+  ipm_solver_settings.reg_prim = 1e-12;
+  ipm_solver_settings.warm_start = 1;
+  ipm_solver_settings.pred_corr = 1;
+  ipm_solver_settings.ric_alg = 0;
+  ipm_solver_settings.split_step = 1;
 
-  hpipm::OcpQpSolution sol;
-  const auto sol_err_msg = sol.checkSize(dim);
-  if (!sol_err_msg.empty()) {
-    for (const auto& e : sol_err_msg) {
-      std::cout << e << std::endl;
-    }
-    return 1;
-  }
-  sol.createHpipmData(dim);
-
-  hpipm::OcpQpIpmSolver solver;
-  solver.createHpipmData(dim, arg);
+  hpipm::OcpQpSolution solution(dim);
+  hpipm::OcpQpIpmSolver solver(dim, ipm_solver_settings);
 
   Eigen::VectorXd u0(4);
   Eigen::VectorXd x(12);
   x << 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0;
 
-  // const int num_tests = 100;
-  // const auto begin = std::chrono::high_resolution_clock::now();
-  // for (int test=0; test<num_tests; ++test) {
-
   const int sim_steps = 50;
   for (int t=0; t<sim_steps; ++t) {
     std::cout << "t: " << t << ", x: " << x.transpose() << std::endl;
     qp.x0 = x;
-    qp.createHpipmData(dim);
-    if (solver.solve(qp, sol, arg) != hpipm::HpipmStatus::Success) return 1;
-    sol.getSolutionFromHpipm(dim);
-    u0 = sol.u[0];
+    if (solver.solve(qp, solution) != hpipm::HpipmStatus::Success) return 1;
+    u0 = solution.u[0];
     x = A * x + B * u0 + b;
   }
-
-  // }
-  // const auto end = std::chrono::high_resolution_clock::now();
-  // const auto timing = std::chrono::duration<double, std::milli>(end - begin);
-  // std::cout << "CPU time: " << timing.count() << " [ms]" << std::endl;
 
   std::cout << "t: " << sim_steps << ", x: " << x.transpose() << std::endl;
   return 0;

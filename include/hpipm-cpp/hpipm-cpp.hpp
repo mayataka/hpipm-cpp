a76ee92a4554fa2c4f46a5d27a11210fbfe6e20e
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-08-24 13:52:44 +0900

    remove uneused functions

--- include/hpipm-cpp/hpipm-cpp.hpp
+++ include/hpipm-cpp/hpipm-cpp.hpp
@@ -7,7 +7,6 @@
 #include "hpipm-cpp/ocp_qp_ipm_solver_settings.hpp"
 #include "hpipm-cpp/ocp_qp_ipm_solver_statistics.hpp"
 #include "hpipm-cpp/ocp_qp_ipm_solver.hpp"
-#include "hpipm-cpp/utils.hpp"
 
 
 #endif // HPIPM_CPP_HPIPM_CPP_HPP_

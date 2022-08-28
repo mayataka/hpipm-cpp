f6a8e7216f8d3e32b8171f0cdd41fa5c410c9566
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-08-28 13:38:44 +0900

    add move constructor and move assign operator

--- src/d_ocp_qp_wrapper.cpp
+++ src/d_ocp_qp_wrapper.cpp
@@ -25,6 +25,7 @@ d_ocp_qp_wrapper::~d_ocp_qp_wrapper() {
   if (memory_) {
     free(memory_);
     memory_ = nullptr;
+    memsize_ = 0;
   }
 }
 
@@ -39,6 +40,61 @@ d_ocp_qp_wrapper& d_ocp_qp_wrapper::operator=(const d_ocp_qp_wrapper& other) {
   if (this != &other) {
     copy(other);
   }
+  return *this;
+}
+
+
+d_ocp_qp_wrapper::d_ocp_qp_wrapper(d_ocp_qp_wrapper&& other) noexcept
+  : dim_(other.dim_),
+    ocp_qp_hpipm_(other.ocp_qp_hpipm_),
+    memory_(other.memory_),
+    memsize_(other.memsize_) {
+  other.ocp_qp_hpipm_.dim = nullptr;
+  other.ocp_qp_hpipm_.BAbt = nullptr;
+  other.ocp_qp_hpipm_.RSQrq = nullptr;
+  other.ocp_qp_hpipm_.DCt = nullptr;
+  other.ocp_qp_hpipm_.b = nullptr;
+  other.ocp_qp_hpipm_.rqz = nullptr;
+  other.ocp_qp_hpipm_.d = nullptr;
+  other.ocp_qp_hpipm_.d_mask = nullptr;
+  other.ocp_qp_hpipm_.m = nullptr;
+  other.ocp_qp_hpipm_.Z = nullptr;
+  other.ocp_qp_hpipm_.idxb = nullptr;
+  other.ocp_qp_hpipm_.idxs_rev = nullptr;
+  other.ocp_qp_hpipm_.idxe = nullptr;
+  other.ocp_qp_hpipm_.diag_H_flag = nullptr;
+  other.ocp_qp_hpipm_.memsize = 0;
+}
+
+
+d_ocp_qp_wrapper& d_ocp_qp_wrapper::operator=(d_ocp_qp_wrapper&& other) noexcept {
+  if (this == &other) return *this;
+
+  if (memory_) {
+    free(memory_);
+    memory_ = nullptr;
+    memsize_ = 0;
+  }
+  dim_ = other.dim_;
+  ocp_qp_hpipm_ = other.ocp_qp_hpipm_;
+  memory_ = other.memory_;
+  memsize_ = other.memsize_;
+
+  other.ocp_qp_hpipm_.dim = nullptr;
+  other.ocp_qp_hpipm_.BAbt = nullptr;
+  other.ocp_qp_hpipm_.RSQrq = nullptr;
+  other.ocp_qp_hpipm_.DCt = nullptr;
+  other.ocp_qp_hpipm_.b = nullptr;
+  other.ocp_qp_hpipm_.rqz = nullptr;
+  other.ocp_qp_hpipm_.d = nullptr;
+  other.ocp_qp_hpipm_.d_mask = nullptr;
+  other.ocp_qp_hpipm_.m = nullptr;
+  other.ocp_qp_hpipm_.Z = nullptr;
+  other.ocp_qp_hpipm_.idxb = nullptr;
+  other.ocp_qp_hpipm_.idxs_rev = nullptr;
+  other.ocp_qp_hpipm_.idxe = nullptr;
+  other.ocp_qp_hpipm_.diag_H_flag = nullptr;
+  other.ocp_qp_hpipm_.memsize = 0;
   return *this;
 }
 

c33e68fa80114fbd775360b479e69f2fc3174c52
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-08-28 13:15:39 +0900

    add move constructor and move assign operator

--- src/d_ocp_qp_dim_wrapper.cpp
+++ src/d_ocp_qp_dim_wrapper.cpp
@@ -26,6 +26,7 @@ d_ocp_qp_dim_wrapper::~d_ocp_qp_dim_wrapper() {
   if (memory_) {
     free(memory_);
     memory_ = nullptr;
+    memsize_ = 0;
   }
 }
 
@@ -39,6 +40,62 @@ d_ocp_qp_dim_wrapper::d_ocp_qp_dim_wrapper(const d_ocp_qp_dim_wrapper& other)
 d_ocp_qp_dim_wrapper& d_ocp_qp_dim_wrapper::operator=(const d_ocp_qp_dim_wrapper& other) {
   if (this != &other) {
     copy(other);
+  }
+  return *this;
+}
+
+
+d_ocp_qp_dim_wrapper::d_ocp_qp_dim_wrapper(d_ocp_qp_dim_wrapper&& other) noexcept
+  : ocp_qp_dim_hpipm_(other.ocp_qp_dim_hpipm_),
+    memory_(other.memory_),
+    memsize_(other.memsize_) {
+  other.ocp_qp_dim_hpipm_.nx   = nullptr;
+  other.ocp_qp_dim_hpipm_.nu   = nullptr;
+  other.ocp_qp_dim_hpipm_.nb   = nullptr;
+  other.ocp_qp_dim_hpipm_.nbx  = nullptr;
+  other.ocp_qp_dim_hpipm_.nbu  = nullptr;
+  other.ocp_qp_dim_hpipm_.ng   = nullptr;
+  other.ocp_qp_dim_hpipm_.ns   = nullptr;
+  other.ocp_qp_dim_hpipm_.nsbx = nullptr;
+  other.ocp_qp_dim_hpipm_.nsbu = nullptr;
+  other.ocp_qp_dim_hpipm_.nsg  = nullptr;
+  other.ocp_qp_dim_hpipm_.nbxe = nullptr;
+  other.ocp_qp_dim_hpipm_.nbue = nullptr;
+  other.ocp_qp_dim_hpipm_.nge  = nullptr;
+  other.ocp_qp_dim_hpipm_.N = 0;
+  other.ocp_qp_dim_hpipm_.memsize = 0;
+  other.memory_ = nullptr;
+  other.memsize_ = 0;
+}
+
+
+d_ocp_qp_dim_wrapper& d_ocp_qp_dim_wrapper::operator=(d_ocp_qp_dim_wrapper&& other) noexcept {
+  if (this != &other) {
+    if (memory_) {
+      free(memory_);
+      memory_ = nullptr;
+      memsize_ = 0;
+    }
+    ocp_qp_dim_hpipm_ = other.ocp_qp_dim_hpipm_;
+    memory_ = other.memory_;
+    memsize_ = other.memsize_;
+    other.ocp_qp_dim_hpipm_.nx   = nullptr;
+    other.ocp_qp_dim_hpipm_.nu   = nullptr;
+    other.ocp_qp_dim_hpipm_.nb   = nullptr;
+    other.ocp_qp_dim_hpipm_.nbx  = nullptr;
+    other.ocp_qp_dim_hpipm_.nbu  = nullptr;
+    other.ocp_qp_dim_hpipm_.ng   = nullptr;
+    other.ocp_qp_dim_hpipm_.ns   = nullptr;
+    other.ocp_qp_dim_hpipm_.nsbx = nullptr;
+    other.ocp_qp_dim_hpipm_.nsbu = nullptr;
+    other.ocp_qp_dim_hpipm_.nsg  = nullptr;
+    other.ocp_qp_dim_hpipm_.nbxe = nullptr;
+    other.ocp_qp_dim_hpipm_.nbue = nullptr;
+    other.ocp_qp_dim_hpipm_.nge  = nullptr;
+    other.ocp_qp_dim_hpipm_.N = 0;
+    other.ocp_qp_dim_hpipm_.memsize = 0;
+    other.memory_ = nullptr;
+    other.memsize_ = 0;
   }
   return *this;
 }

1c6236b8a15374d7c04e7dbc49a72a72b09424cb
Sotaro Katayama <ihtf4ta7f@gmail.com>
2022-08-29 09:21:13 +0900

    add tests

--- test/d_ocp_qp_dim_wrapper.cpp
+++ test/d_ocp_qp_dim_wrapper.cpp
@@ -24,6 +24,7 @@ TEST_F(d_ocp_qp_dim_wrapper_test, copy) {
   const unsigned int N = 10;
   d_ocp_qp_dim_wrapper dim(N);
   EXPECT_EQ(dim.get()->N, 10);
+
   for (unsigned int i=0; i<=10; ++i) {
     dim.get()->nx[i]   = distr(eng);
     dim.get()->nu[i]   = distr(eng);
@@ -104,6 +105,101 @@ TEST_F(d_ocp_qp_dim_wrapper_test, copy) {
   EXPECT_TRUE(dim3.get()->nbxe != dim.get()->nbxe);
   EXPECT_TRUE(dim3.get()->nbue != dim.get()->nbue);
   EXPECT_TRUE(dim3.get()->nge != dim.get()->nge);
+}
+
+
+TEST_F(d_ocp_qp_dim_wrapper_test, move) {
+  std::random_device rd;
+  std::default_random_engine eng(rd());
+  std::uniform_int_distribution<int> distr(0, 10);
+
+  const unsigned int N = 10;
+  d_ocp_qp_dim_wrapper dim(N);
+  EXPECT_EQ(dim.get()->N, 10);
+
+  for (unsigned int i=0; i<=10; ++i) {
+    dim.get()->nx[i]   = distr(eng);
+    dim.get()->nu[i]   = distr(eng);
+    dim.get()->nb[i]   = distr(eng);
+    dim.get()->nbx[i]  = distr(eng);
+    dim.get()->nbu[i]  = distr(eng);
+    dim.get()->ng[i]   = distr(eng);
+    dim.get()->ns[i]   = distr(eng);
+    dim.get()->nsbx[i] = distr(eng);
+    dim.get()->nsbu[i] = distr(eng);
+    dim.get()->nsg[i]  = distr(eng);
+    dim.get()->nbxe[i] = distr(eng);
+    dim.get()->nbue[i] = distr(eng);
+    dim.get()->nge[i]  = distr(eng);
+  }
+
+  d_ocp_qp_dim_wrapper dim2(dim);
+
+  d_ocp_qp_dim_wrapper dim3(std::move(dim2));
+  EXPECT_EQ(dim3.get()->N, dim.get()->N);
+  for (unsigned int i=0; i<=10; ++i) {
+    EXPECT_EQ(dim3.get()->nx[i],   dim.get()->nx[i]);
+    EXPECT_EQ(dim3.get()->nu[i],   dim.get()->nu[i]);
+    EXPECT_EQ(dim3.get()->nbx[i],  dim.get()->nbx[i]);
+    EXPECT_EQ(dim3.get()->nbu[i],  dim.get()->nbu[i]);
+    EXPECT_EQ(dim3.get()->ng[i],   dim.get()->ng[i]);
+    EXPECT_EQ(dim3.get()->ns[i],   dim.get()->ns[i]);
+    EXPECT_EQ(dim3.get()->nsbx[i], dim.get()->nsbx[i]);
+    EXPECT_EQ(dim3.get()->nsbu[i], dim.get()->nsbu[i]);
+    EXPECT_EQ(dim3.get()->nsg[i],  dim.get()->nsg[i]);
+    EXPECT_EQ(dim3.get()->nbxe[i], dim.get()->nbxe[i]);
+    EXPECT_EQ(dim3.get()->nbue[i], dim.get()->nbue[i]);
+    EXPECT_EQ(dim3.get()->ng[i],   dim.get()->ng[i]);
+  }
+  EXPECT_EQ(dim3.get()->memsize, dim.get()->memsize);
+  EXPECT_TRUE(dim3.get() != dim.get());
+  EXPECT_TRUE(dim3.get()->nx != dim.get()->nx);
+  EXPECT_TRUE(dim3.get()->nu != dim.get()->nu);
+  EXPECT_TRUE(dim3.get()->nb != dim.get()->nb);
+  EXPECT_TRUE(dim3.get()->nbx != dim.get()->nbx);
+  EXPECT_TRUE(dim3.get()->nbu != dim.get()->nbu);
+  EXPECT_TRUE(dim3.get()->ng != dim.get()->ng);
+  EXPECT_TRUE(dim3.get()->ns != dim.get()->ns);
+  EXPECT_TRUE(dim3.get()->nsbx != dim.get()->nsbx);
+  EXPECT_TRUE(dim3.get()->nsbu != dim.get()->nsbu);
+  EXPECT_TRUE(dim3.get()->nsg != dim.get()->nsg);
+  EXPECT_TRUE(dim3.get()->nbxe != dim.get()->nbxe);
+  EXPECT_TRUE(dim3.get()->nbue != dim.get()->nbue);
+  EXPECT_TRUE(dim3.get()->nge != dim.get()->nge);
+
+  d_ocp_qp_dim_wrapper dim4(dim);
+  d_ocp_qp_dim_wrapper dim5;
+  dim5 = std::move(dim4);
+  EXPECT_EQ(dim5.get()->N, dim.get()->N);
+  for (unsigned int i=0; i<=10; ++i) {
+    EXPECT_EQ(dim5.get()->nx[i],   dim.get()->nx[i]);
+    EXPECT_EQ(dim5.get()->nu[i],   dim.get()->nu[i]);
+    EXPECT_EQ(dim5.get()->nbx[i],  dim.get()->nbx[i]);
+    EXPECT_EQ(dim5.get()->nbu[i],  dim.get()->nbu[i]);
+    EXPECT_EQ(dim5.get()->ng[i],   dim.get()->ng[i]);
+    EXPECT_EQ(dim5.get()->ns[i],   dim.get()->ns[i]);
+    EXPECT_EQ(dim5.get()->nsbx[i], dim.get()->nsbx[i]);
+    EXPECT_EQ(dim5.get()->nsbu[i], dim.get()->nsbu[i]);
+    EXPECT_EQ(dim5.get()->nsg[i],  dim.get()->nsg[i]);
+    EXPECT_EQ(dim5.get()->nbxe[i], dim.get()->nbxe[i]);
+    EXPECT_EQ(dim5.get()->nbue[i], dim.get()->nbue[i]);
+    EXPECT_EQ(dim5.get()->ng[i],   dim.get()->ng[i]);
+  }
+  EXPECT_EQ(dim5.get()->memsize, dim.get()->memsize);
+  EXPECT_TRUE(dim5.get() != dim.get());
+  EXPECT_TRUE(dim5.get()->nx != dim.get()->nx);
+  EXPECT_TRUE(dim5.get()->nu != dim.get()->nu);
+  EXPECT_TRUE(dim5.get()->nb != dim.get()->nb);
+  EXPECT_TRUE(dim5.get()->nbx != dim.get()->nbx);
+  EXPECT_TRUE(dim5.get()->nbu != dim.get()->nbu);
+  EXPECT_TRUE(dim5.get()->ng != dim.get()->ng);
+  EXPECT_TRUE(dim5.get()->ns != dim.get()->ns);
+  EXPECT_TRUE(dim5.get()->nsbx != dim.get()->nsbx);
+  EXPECT_TRUE(dim5.get()->nsbu != dim.get()->nsbu);
+  EXPECT_TRUE(dim5.get()->nsg != dim.get()->nsg);
+  EXPECT_TRUE(dim5.get()->nbxe != dim.get()->nbxe);
+  EXPECT_TRUE(dim5.get()->nbue != dim.get()->nbue);
+  EXPECT_TRUE(dim5.get()->nge != dim.get()->nge);
 }
 
 } // namespace hpipm
